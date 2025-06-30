ea9524f4aa5b26d26ab0eaeba9a7bdc1c5adc219
Ivan G <grikoivan@yandex.ru>
2025-07-01 00:08:56 +0300

    ФЗ 16

--- server/src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -18,10 +18,10 @@ import ru.practicum.shareit.user.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+//import static org.junit.jupiter.api.Assertions.fail;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @DataJpaTest
